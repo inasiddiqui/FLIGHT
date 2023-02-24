3d499b76dcb7012588e0eda33b8fb8b33ed6d1a8
ina <siddi352@newschool.edu>
2023-02-23 20:14:22 -0500

    Made edits in hover styles

---
+++ assets/expand.js
@@ -0,0 +1,9 @@
+
+    // document.querySelectorAll(".grid-item");
+    // var allItems= document.querySelectorAll('.grid-item')
+    // console.log(allItems)
+    // allItems.forEach(item => { item.addEventListener(“click”, expand(){
+    //     console.log(item + “was clicked”);
+
+    // }) } )
+    
